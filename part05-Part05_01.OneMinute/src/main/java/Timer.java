4a8b13d877200a193c631db9e508e3e7a97ba973
stefanpython <stefanhtml@yahoo.com>
2024-06-17 16:48:49 +0300

    Complete exercise 2 part 5

--- part05-Part05_01.OneMinute/src/main/java/Timer.java
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -1,31 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author tao
- */
 public class Timer {
     private ClockHand seconds;
     private ClockHand hundredths;
     
     public Timer() {
-        this.seconds = new ClockHand(60);
-        this.hundredths = new ClockHand(100);
+        this.seconds = new ClockHand(60);  // Initialize seconds with a maximum value of 60
+        this.hundredths = new ClockHand(100);  // Initialize hundredths with a maximum value of 100
     }
     
     public void advance() {
-        this.hundredths.advance();
+        this.hundredths.advance();  // Advance hundredths
         
-        if(this.hundredths.value() == 0) {
-            this.seconds.advance();
+        if (this.hundredths.value() == 0) {  // When hundredths roll over
+            this.seconds.advance();  // Advance seconds
         }
     }
     
     public String toString() {
-        return seconds + ": " + hundredths;
+        return String.format("%02d:%02d", this.seconds.value(), this.hundredths.value());  // Format the output as "seconds:hundredths"
     }
 }
