5d847d4b26b5b913ed3e21c611519dac02fe4e00
stefanpython <stefanhtml@yahoo.com>
2024-06-18 16:38:25 +0300

    complete part5 exercise 7

--- part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
+++ part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
@@ -8,6 +8,7 @@ public class NullPointerExceptionProgram {
         // the NullPointerException -error
 
         ArrayList<String> lines = new ArrayList<>();
+        lines = null;
         lines.add("Never has a man influenced physics so profoundly as Niels Bohr in the early 1900's");
         lines.add("Going back to this time period, little was known about atomic structure; Bohr set out");
         lines.add("to end the obscurity of physics. However, things didn't come easy for Bohr. He had to");
@@ -16,10 +17,12 @@ public class NullPointerExceptionProgram {
         lines.add("up with his quantum theory while studying...");
 
         int index = 0;
+        
         while (index < lines.size()) {
             System.out.println(lines.get(index));
             index++;
         }
 
+        
     }
 }
