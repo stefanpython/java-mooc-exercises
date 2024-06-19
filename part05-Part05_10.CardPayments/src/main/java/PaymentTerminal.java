67b5dccf760be4bc6353474fbd46e502abeee9bb
stefanpython <stefanhtml@yahoo.com>
2024-06-19 16:26:36 +0300

    complete part5 exercise 10

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,30 +1,85 @@
 
 public class PaymentTerminal {
 
-
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
-
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000.0;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
+
     }
 
     public double eatAffordably(double payment) {
+
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double mealCost = 2.50;
+
+        if (payment >= mealCost) {
+            this.money += mealCost;
+            this.affordableMeals++;
+
+            return payment - mealCost;
+        } else {
+            return payment;
+        }
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double mealCost = 4.30;
+        if (payment >= mealCost) {
+            this.money += mealCost;
+            this.heartyMeals++;
+            return payment - mealCost;
+        } else {
+            return payment;
+        }
     }
 
+    public boolean eatAffordably(PaymentCard card) {
+        // an affordable meal costs 2.50 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
+        double mealCost = 2.50;
+        if (card.balance() >= mealCost) {
+            card.takeMoney(mealCost);
+            this.affordableMeals++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        // a hearty meal costs 4.30 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
+        double mealCost = 4.30;
+        if (card.balance() >= mealCost) {
+            card.takeMoney(mealCost);
+            this.heartyMeals++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if(sum > 0) {
+            card.addMoney(sum);
+            this.money += sum;
+        }
+        
+        
+    }
 
     @Override
     public String toString() {
