cd16fc4486cb60b5a70a200b5aa9149460280953
stefanpython <stefanhtml@yahoo.com>
2024-06-20 14:51:34 +0300

    complete part5 exercise 11

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -4,11 +4,34 @@ public class Apartment {
     private int rooms;
     private int squares;
     private int princePerSquare;
+    private int totalPrice;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
+        this.totalPrice = this.squares * this.princePerSquare;
+    }
+    
+    public boolean largerThan(Apartment compared) {
+        if(this.squares > compared.squares) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public int priceDifference(Apartment compared) {
+        return Math.abs(this.totalPrice - compared.totalPrice);
+        
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        if(this.totalPrice > compared.totalPrice) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
