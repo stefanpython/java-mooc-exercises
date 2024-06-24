e941aa5320793a966cba0029e0faf4d058818759
stefanpython <stefanhtml@yahoo.com>
2024-06-24 16:16:02 +0300

    complete part5 exercise 17

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -1,14 +1,12 @@
-
 public class Money {
 
     private final int euros;
     private final int cents;
 
     public Money(int euros, int cents) {
-
         if (cents > 99) {
-            euros = euros + cents / 100;
-            cents = cents % 100;
+            euros += cents / 100;
+            cents %= 100;
         }
 
         this.euros = euros;
@@ -16,20 +14,53 @@ public class Money {
     }
 
     public int euros() {
-        return this.euros;
+        return euros;
     }
 
     public int cents() {
-        return this.cents;
+        return cents;
     }
 
     public String toString() {
-        String zero = "";
-        if (this.cents < 10) {
-            zero = "0";
-        }
-
-        return this.euros + "." + zero + this.cents + "e";
+        String centString = (cents < 10) ? "0" + cents : String.valueOf(cents);
+        return euros + "." + centString + "e";
     }
 
+    public Money plus(Money addition) {
+        int newEuros = this.euros + addition.euros;
+        int newCents = this.cents + addition.cents;
+
+        if (newCents >= 100) {
+            newEuros += newCents / 100;
+            newCents %= 100;
+        }
+
+        return new Money(newEuros, newCents);
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        } else if (this.euros == compared.euros && this.cents < compared.cents) {
+            return true;
+        }
+        return false;
+    }
+
+    public Money minus(Money decreaser) {
+        int newEuros = this.euros - decreaser.euros;
+        int newCents = this.cents - decreaser.cents;
+
+        if (newCents < 0) {
+            newEuros -= 1;
+            newCents += 100;
+        }
+
+        if (newEuros < 0) {
+            newEuros = 0;
+            newCents = 0;
+        }
+
+        return new Money(newEuros, newCents);
+    }
 }
