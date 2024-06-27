bb2f265d41b75591d6baac4c54ab396926352dde
stefanpython <stefanhtml@yahoo.com>
2024-06-27 14:46:23 +0300

    complete part6 exercise 10

---
+++ part06-Part06_10.TodoList/src/main/java/UserInterface.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private TodoList list;
+    private Scanner scanner;
+
+    public UserInterface(TodoList list, Scanner scanner) {
+        this.list = list;
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("stop")) {
+                break;
+            } else if (command.equals("add")) {
+                System.out.print("To add: ");
+                String task = scanner.nextLine();
+                list.add(task);
+            } else if (command.equals("list")) {
+                list.print();
+            } else if (command.equals("remove")) {
+                System.out.print("Which one is removed? ");
+                int taskNumber = Integer.parseInt(scanner.nextLine());
+                list.remove(taskNumber);
+            }
+        }
+    }
+}
