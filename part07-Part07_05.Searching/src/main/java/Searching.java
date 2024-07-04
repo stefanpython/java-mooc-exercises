1b2532a39f2e6465c16bfd5de9a26f7d0fbd6351
stefanpython <stefanhtml@yahoo.com>
2024-07-04 13:28:17 +0300

    complete part7 exercise 5

--- part07-Part07_05.Searching/src/main/java/Searching.java
+++ part07-Part07_05.Searching/src/main/java/Searching.java
@@ -44,10 +44,30 @@ public class Searching {
     }
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
+        for(int i = 0; i < books.size(); i++){
+            if(books.get(i).getId() == searchedId) {
+                return i;
+            }
+        }
         return -1;
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
+        int start = 0;
+        int end = books.size() - 1;
+        
+        while(start <= end) {
+            int mid = (end + start) / 2;
+            
+            if(books.get(mid).getId() == searchedId) {
+                return mid;
+            } else if (books.get(mid).getId() < searchedId) {
+                start = mid + 1;
+            } else if(books.get(mid).getId() > searchedId) {
+                end = mid - 1;
+            }
+        }
+        
         return -1;
     }
 }
