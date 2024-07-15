6b268f87b54414f71509c2e9dc87904631f70372
stefanpython <stefanhtml@yahoo.com>
2024-07-15 16:10:33 +0300

    complete exercise 12 part8

--- part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
+++ part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
@@ -51,4 +51,33 @@ public class SimpleDate {
         return this.year - other.year - yearRemoved;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hash = 1;
+        hash = prime * hash + day;
+        hash = prime * hash + month;
+        hash = prime * hash + year;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SimpleDate other = (SimpleDate) obj;
+        if (this.year != other.year) {
+            return false;
+        }
+        return true;
+    }
+
+    
 }
