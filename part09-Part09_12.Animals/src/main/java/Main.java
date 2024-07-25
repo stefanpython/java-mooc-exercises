031bf7836d5284a01048faad4fbae19be17945bb
stefanpython <stefanhtml@yahoo.com>
2024-07-25 16:36:57 +0300

    complete exercise 12 part9

--- part09-Part09_12.Animals/src/main/java/Main.java
+++ part09-Part09_12.Animals/src/main/java/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         // you can test how your classes work here
-
+//        Dog dog = new Dog();
+//        dog.bark();
+//        dog.eat();
+//
+//        Dog fido = new Dog("Fido");
+//        fido.bark();
     }
 
 }
