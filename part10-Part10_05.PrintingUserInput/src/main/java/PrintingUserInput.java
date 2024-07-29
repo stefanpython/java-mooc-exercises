a0e5f02df0b1f9e422cdda2d57c6a96fd05931d8
stefanpython <stefanhtml@yahoo.com>
2024-07-29 16:44:15 +0300

    complete exercise 5 part10

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,21 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
+        while(true) {
+            String input = scanner.nextLine();
+            
+            list.add(input);
+            
+            if(input.equals("")) {
+                String combined = list.stream()
+                        .reduce("", (prev, next) -> prev + next + "\n" );
+                
+                System.out.println(combined);
+                break;
+                
+            }
+        }
     }
 }
