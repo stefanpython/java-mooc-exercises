052bbb7f6c07d54888f5970f755aa8f52102823d
stefanpython <stefanhtml@yahoo.com>
2024-07-30 11:26:07 +0300

    complete exercise 6 part10

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,30 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (true) {
+            Integer input = Integer.valueOf(scanner.nextLine());
+
+            if (input < 0) {
+                break;
+            }
+
+            list.add(input);
+
+        }
+
+        ArrayList<Integer> values = list.stream()
+                .filter(value -> value <= 5 && value >= 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+        
+        System.out.println(values);
     }
 }
