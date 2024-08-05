464fb4c1de213b874d897898489b6d5bf63c4ed1
stefanpython <stefanhtml@yahoo.com>
2024-08-05 14:32:47 +0300

    complete exercise 14 part10

--- part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,11 +1,40 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Book> books = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Input the name of the book, empty stops: ");
+
+            String first = scanner.nextLine();
+            if (first.equals("")) {
+                break;
+            }
+
+            System.out.println("Input the age recommendation:");
+            int second = Integer.valueOf(scanner.nextLine());
+
+            books.add(new Book(first, second));
+        }
+
+        System.out.println(books.size() + " books in total.");
+
+        System.out.println("\nBooks:");
+        
+        Comparator<Book> comparator = Comparator
+                .comparing(Book::getAge)
+                .thenComparing(Book::getName);
+        
+        Collections.sort(books, comparator);
+
+        for (Book book : books) {
+            System.out.println(book);
+        }
     }
 
 }
