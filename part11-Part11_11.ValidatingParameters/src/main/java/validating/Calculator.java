8d95aa332a9f6c4b5318b35b5f085d77b884421d
stefanpython <stefanhtml@yahoo.com>
2024-08-13 15:03:14 +0300

    complete xercise 11 part11

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,6 +3,10 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
+        
+        if(num < 0) {
+            throw new IllegalArgumentException("Number must be 0 or greater.");
+        }
 
         int answer = 1;
         for (int i = 1; i <= num; i++) {
@@ -13,6 +17,12 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
+        
+        if(setSize < 0 || subsetSize < 0 || subsetSize > setSize) {
+            throw new IllegalArgumentException("Parameters must be non-negative integers.");
+        }
+        
+        
 
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -7,7 +7,16 @@ public class Person {
 
     public Person(String name, int age) {
 
+        if (name == null || name.trim().isEmpty() || name.length() > 40) {
+            throw new IllegalArgumentException("Name must not be null or empty and length must not be over 40 characters.");
+        }
+
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Age must be above 0 and lesser than 120.");
+        }
+
         this.name = name;
+
         this.age = age;
     }
 
