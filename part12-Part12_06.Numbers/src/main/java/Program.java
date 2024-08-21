c04130276e07f4598aa669b0a8d653d86ff29df9
stefanpython <stefanhtml@yahoo.com>
2024-08-21 14:13:31 +0300

    complete exercise 6 part12

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,18 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        Random random = new Random();
+        
+        System.out.println("How many random numbers should be printed?");
+        
+        int nextLine = Integer.valueOf(scanner.nextLine());
+        
+        for(int i = 0; i < nextLine; i++) {
+            int randomNumber = random.nextInt(11);
+            
+            System.out.println(randomNumber);
+        }
     }
 
 }
