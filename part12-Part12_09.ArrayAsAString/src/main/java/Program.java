21b71c19c212ed15e967e1b986767b9d42651a68
stefanpython <stefanhtml@yahoo.com>
2024-08-22 14:49:37 +0300

    complete exercise 9 part12

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -1,8 +1,24 @@
 
+
 public class Program {
 
     public static void main(String[] args) {
         // Test your method here
+        
+    }
+    
+    public static String arrayAsString(int[][] array) {
+        StringBuilder numbers = new StringBuilder();
+        
+        for(int i = 0; i < array.length; i++) {
+            for(int j = 0; j < array[i].length; j++) {
+                numbers.append(array[i][j]);
+            }
+            if (i < array.length - 1) {
+                numbers.append("\n");
+            }
+        }
+        return numbers.toString();
     }
 
 }
