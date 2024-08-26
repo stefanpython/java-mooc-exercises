9527a5bb877f92ca699b752c101574cb06f22837
stefanpython <stefanhtml@yahoo.com>
2024-08-26 17:36:37 +0300

    complete exercise 6 part13

--- part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -2,13 +2,10 @@ package textstatistics;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TextStatisticsApplication extends Application{
